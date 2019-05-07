b00b945e67324867f742eda985e891d4898f1b4a
PowWow Admin <admin@studio.dev.powwowinc.net>
2019-05-07 13:11:34 +0000

    [Studio] Delete screen "home"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -1,14 +1,8 @@
 /***  Generated file, do not change.  */
 import { MenuComponent } from './menu/menu';
-import { home_PhonePortrait } from '../pages/home/PhonePortrait/home';
 export class Screens {
   static declarations = [
-    MenuComponent,
-    home_PhonePortrait
+    MenuComponent
   ];
-  static mapping = {
-    'home': {
-      PhonePortrait: home_PhonePortrait
-    }
-  }
+  static mapping = {}
 }
