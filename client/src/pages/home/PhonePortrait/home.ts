1f29c28977d0b05605afa11426328f0cdd890ac7
testuser <test@mail.com>
2019-05-07 15:36:00 +0000

    [Studio] Add screen "Home"

---
+++ client/src/pages/home/PhonePortrait/home.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Screen } from 'app/screen';
+declare var window: any;
+
+@Component({
+  selector: 'screen-home-phoneportrait',
+  templateUrl: 'home.html'
+})
+export class home_PhonePortrait extends Screen {
+  data: any;
+
+  ngOnInit(): void {
+    super.ngOnInit();
+    // Logic to run when the screen loads goes here.
+  }
+
+  ngOnDestroy(): void {
+    super.ngOnDestroy();
+    // Logic to run when the screen unloads goes here.
+  }
+
+  onDataLoad(data: any): void {
+    // Logic to run when the screen's data is updated goes here.
+  }
+  onBackButton(): boolean {
+    //(Android) returns :
+    // true - handle the event in App Hooks
+    // false - stop the event propogation
+      return true;
+  }
+}
